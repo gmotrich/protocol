4364997a0a01ec1f097984796589fce470b46a0f
gmotrich <demetrashvili.dm@gmail.com>
2023-10-01 11:47:25 +0300

    end

--- src/main/java/ru/gmotrich/protocol/entity/message/Message.java
+++ src/main/java/ru/gmotrich/protocol/entity/message/Message.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Message {
+public class Message implements Serializable {
     private Header header;
-    private byte[] data;
+    private Object data;
 
 }
